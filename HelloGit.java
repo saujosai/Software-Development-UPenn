9682658c7a8156018c9d0bc69e14bc875dbc0aea
Saul Saitowitz <sjsaitowitz@gmail.com>
2019-06-16 18:28:00 -0400

    I think we figure it out!

--- HelloGit.java
+++ HelloGit.java
@@ -2,7 +2,7 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 
-        System.out.println("Hello from git collaborator, malmassy");
+        System.out.println("Malmassy, you finally helped me figure this out! Thanks!");
 
 	}
 
